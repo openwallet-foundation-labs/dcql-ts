4ada8a0c7a9f7a3e5ae0b00d1a713287457a98b6
Martin Auer <martin.auer97@gmail.com>
2024-10-16 16:18:28 +0200

    fix: tooling Signed-off-by: Martin Auer <martin.auer97@gmail.com>

--- vp-query/eslint.config.js
+++ vp-query/eslint.config.js
@@ -1,4 +1,4 @@
-import baseConfig, { restrictEnvAccess } from '@protokoll/eslint-config/base';
+import baseConfig, { restrictEnvAccess } from '@ausweis/eslint-config/base';
 
 /** @type {import('typescript-eslint').Config} */
 export default [
