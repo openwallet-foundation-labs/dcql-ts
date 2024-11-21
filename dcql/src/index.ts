282d3cf6a5e559f2c1a5663c109ff765844011ee
Martin Auer <martin.auer97@gmail.com>
2024-11-21 17:15:01 +0100

    docs(changeset): fix export Signed-off-by: Martin Auer <martin.auer97@gmail.com>

--- dcql/src/index.ts
+++ dcql/src/index.ts
@@ -1,5 +1,5 @@
 export {
-  DcqlPresentationQuery as DcqlPresentationQueryResult,
+  DcqlPresentationQuery,
   DcqlPresentationRecord,
 } from './dcql-presentation/index.js';
 export { DcqlQueryResult } from './dcql-query-result/m-dcql-query-result.js';
