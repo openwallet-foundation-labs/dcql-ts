69417db2d90247031121798638044d0e67bd06fc
Timo Glastra <timo@animo.id>
2025-03-24 12:16:46 +0100

    test for credential sets
    Signed-off-by: Timo Glastra <timo@animo.id>

--- dcql/src/dcql-presentation/dcql-presentation.test.ts
+++ dcql/src/dcql-presentation/dcql-presentation.test.ts
@@ -120,4 +120,75 @@ describe('DCQL presentation with claim sets', () => {
       invalid_matches: undefined,
     })
   })
+
+  test('Correctly handles a presentation with a credential set and multiple claim sets where the first credential set and second claim set matches', () => {
+    const query: DcqlQuery.Output = {
+      credentials: [
+        {
+          id: '8c791a1f-12b4-41fe-a892-236c2887fa8e',
+          format: 'vc+sd-jwt',
+          meta: { vct_values: ['PersonIdentificationData'] },
+          claims: [
+            { id: 'a', path: ['given_name'] },
+            { id: 'b', path: ['family_name'] },
+            { id: 'c', path: ['tax_id_code'] },
+          ],
+          claim_sets: [['c'], ['a', 'b']],
+        },
+      ],
+      credential_sets: [
+        {
+          options: [['8c791a1f-12b4-41fe-a892-236c2887fa8e']],
+          required: true,
+        },
+      ],
+    }
+
+    const dcqlPresentation = {
+      '8c791a1f-12b4-41fe-a892-236c2887fa8e': {
+        credential_format: 'vc+sd-jwt',
+        vct: 'PersonIdentificationData',
+        claims: {
+          given_name: { foo: {} },
+          family_name: { bar: {} },
+        },
+      },
+    } as const
+
+    const parsedQuery = DcqlQuery.parse(query)
+    DcqlQuery.validate(parsedQuery)
+
+    const presentationQueryResult = DcqlPresentationResult.fromDcqlPresentation(dcqlPresentation, {
+      dcqlQuery: parsedQuery,
+    })
+
+    expect(presentationQueryResult).toEqual({
+      canBeSatisfied: true,
+      credentials: parsedQuery.credentials,
+      credential_sets: [
+        {
+          matching_options: [['8c791a1f-12b4-41fe-a892-236c2887fa8e']],
+          options: [['8c791a1f-12b4-41fe-a892-236c2887fa8e']],
+          required: true,
+        },
+      ],
+      valid_matches: {
+        '8c791a1f-12b4-41fe-a892-236c2887fa8e': {
+          claim_set_index: 1,
+          presentation_id: '8c791a1f-12b4-41fe-a892-236c2887fa8e',
+          success: true,
+          typed: true,
+          output: {
+            claims: {
+              given_name: { foo: {} },
+              family_name: { bar: {} },
+            },
+            credential_format: 'vc+sd-jwt',
+            vct: 'PersonIdentificationData',
+          },
+        },
+      },
+      invalid_matches: undefined,
+    })
+  })
 })
